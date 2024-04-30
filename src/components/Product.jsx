cb6d7e4dd5659d0d285c69dc8416bc928ca5d63c
Megha-Bathla <bathlamegha6@gmail.com>
2024-05-01 00:40:09 +0530

    create a component and added code

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,41 @@
 import React from "react";
+import { useProducts } from "../context/useProducts";
+import { useCart } from "../context/useCart";
 
 export const Product = () => {
+  const { products, setProducts } = useProducts();
+  const { cart, setCart } = useCart();
+  const handleCart = (key) => {
+    const selectedProduct = products.find((product) => product.key === key);
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.key === key ? { ...product, stock: product.stock - 1 } : product
+      )
+    );
+    setCart([...cart, selectedProduct]);
+  };
   return (
-    <div className="max-w-52 max-h-48 border border-black flex flex-col rounded-md p-4 m-2">
-      <div className="flex-row- ">product name</div>
-      <div>price</div>
-      <div>stock-5</div>
-      <button className="bg-black max-w-24  max-h-10 text-white rounded-md p-2 m-2">
-        Add to cart
-      </button>
+    <div>
+      <div>
+        {products.map((product) => {
+          return (
+            <div
+              key={product.key}
+              className="max-w-52 max-h-48 border border-black flex flex-col rounded-md p-4 m-2"
+            >
+              <div>Name: {product.productName}</div>
+              <div>Price: {product.price}</div>
+              <div>Stock: {product.stock}</div>
+              <button
+                onClick={() => handleCart(product.key)}
+                className="bg-black max-w-24  max-h-10 text-white rounded-md p-2 m-2"
+              >
+                Add to cart
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 export const productList = [
-  { productName: "MacBook", price: 110000, stock: 5 },
-  { productName: "Table", price: 70000, stock: 10 },
-  { productName: "Chair", price: 90000, stock: 12 },
-  { productName: "Headphones", price: 10000, stock: 7 },
-  { productName: "Mobile", price: 50000, stock: 4 },
+  { productName: "MacBook", price: 110000, stock: 5, key: 1 },
+  { productName: "Table", price: 70000, stock: 10, key: 2 },
+  { productName: "Chair", price: 90000, stock: 12, key: 3 },
+  { productName: "Headphones", price: 10000, stock: 7, key: 4 },
+  { productName: "Mobile", price: 50000, stock: 4, key: 5 },
 ];
