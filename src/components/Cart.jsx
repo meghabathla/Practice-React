c5d4fc3a3001098c3e0a8726659c05f1896a2d13
Megha-Bathla <bathlamegha6@gmail.com>
2024-05-01 14:51:00 +0530

    created and added code

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useCart } from "../context/useCart";
 
 export const Cart = () => {
+  const { cart, setCart } = useCart();
+  console.log(cart);
+  const total = cart
+    ? cart.reduce((acc, cur) => {
+        return acc + parseInt(cur.price);
+      }, 0)
+    : 0;
+  console.log(total);
+
   return (
     <div>
       <h3>Cart</h3>
-      <div>Product name</div>
-      <div>price</div>
-      <div>quantity no of products</div>
-      <div>total amount</div>
+      <div className="max-w-52 max-h-48 border border-black flex flex-col rounded-md p-4 m-2">
+        {cart.map((product) => (
+          <div key={product.key}>
+            <div>Name: {product.productName}</div>
+            <div>Price: {product.price}</div>
+          </div>
+        ))}
+        <div>Total bill: {total}</div>
+      </div>
     </div>
   );
 };
